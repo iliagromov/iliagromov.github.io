ccf78843ce619076fc41ca14def1f3b9815284ab
iliagromov <ilia.workproffile@gmail.com>
2023-04-03 00:48:01 +0400

    Релиз 1.0

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -40,31 +40,59 @@ const config: GatsbyConfig = {
         // url:  "http://localhost:8080/graphql",
         baseUrl: `gromov-studio.site`,
         // protocol: `https`,
-        hostingWPCOM: false,
-        useACF: true,
-        auth: {
-            htaccess: {
-              username: process.env.WP_USER,
-              password: process.env.WP_PASSWORD,
-            }
-        },
-        verbose: false,
-        debug: {
-          preview: false,
-          timeBuildSteps: false,
-          throwRefetchErrors: false,
-          // graphql: {
-
-          // },
-          // production: {
-
-          // }
-        },
+        verbose: true,
         develop: {
+          hardCacheMediaFiles: true,
+
           // server wp update  5min times
-          nodeUpdateInterval: 10000,
+          nodeUpdateInterval: 1000,
         },
-        catchLinks: false,
+        debug: {
+          graphql: {
+            writeQueriesToDisk: true,
+            showQueryVarsOnError: true,
+            panicOnError: true,
+          },
+        },
+        useACF: true,
+        
+
+        excludeFieldNames: ['comments', 'blocksJSON', 'previewBlocks', 'previewBlocksJSON'],
+        type: {
+          Post: {
+            limit:
+              process.env.NODE_ENV === `development`
+                ? // The number of posts to fetch on development
+                  30
+                : // ... and on build
+                  null,
+          },
+          Comment: {
+            limit: 0,
+          },
+          MediaItem: {
+            localFile: {
+              requestConcurrency: 5,
+            },
+            lazyNodes: false,
+          },
+        },
+        html: {
+          useGatsbyImage: true,
+          fallbackImageMaxWidth: 200,
+          createStaticFiles: true,
+          imageMaxWidth: 756,
+        },
+        schema: {
+          timeout: 3000000,
+          requestConcurrency: 1,
+          previewRequestConcurrency: 1,
+          perPage: 1,
+        },
+        // production: {
+        //   allow404Images: true,
+        //   hardCacheMediaFiles: true,
+        // },
       },
     }, 
     "gatsby-plugin-image", 
