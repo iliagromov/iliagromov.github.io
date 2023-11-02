507b973c9bb6189d0a1d7afe3f24efe2cae5166b
iliagromov <ilia.workproffile@gmail.com>
2023-11-03 00:01:51 +0300

    Убрал статику

--- src/components/common/Projects/Card.tsx
+++ src/components/common/Projects/Card.tsx
@@ -1,106 +1,88 @@
-import { Link } from 'gatsby';
-import React, { FC } from 'react';
+import { Link } from "gatsby";
+import React, { FC } from "react";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { EffectCards } from "swiper";
-import { GatsbyImage, getImage, StaticImage, } from "gatsby-plugin-image";
+import { GatsbyImage, getImage, StaticImage } from "gatsby-plugin-image";
 
-import { blockProject, projectPage } from '../../../shared/types';
+import { blockProject, projectPage } from "../../../shared/types";
 
 import "swiper/css";
 import "swiper/css/effect-cards";
-import './Card.sass';
+import "./Card.sass";
 
 type CardProps = {
-    blockProject: any,
-}
+  blockProject: any;
+};
 
 const Card: FC<CardProps> = (props) => {
-    const {
-        title,
-        publicData,
-        pagesCount,
-        pages,
-        link
-    } = props.blockProject.frontmatter;
+  const { title, publicData, pagesCount, pages, link } =
+    props.blockProject.frontmatter;
 
-    // console.log(props.blockProject.frontmatter);
+  // console.log(props.blockProject.frontmatter);
 
-    const projectLink = link ? link : '/projects';
+  const projectLink = link ? link : "/projects";
 
-    const pagesAr = pages.map((project: any)=>{
-        const obj1 = project.page[0];
-        const obj2 = project.page[1];
-        const merged = {};
-        Object.keys(obj1).forEach((key) => merged[key] = obj1[key] ? obj1[key] : obj2[key]);
-        return merged
-      })
-    // console.log(pagesAr);
+  const pagesAr = pages.map((project: any) => {
+    const obj1 = project.page[0];
+    const obj2 = project.page[1];
+    const merged = {};
+    Object.keys(obj1).forEach(
+      (key) => (merged[key] = obj1[key] ? obj1[key] : obj2[key])
+    );
+    return merged;
+  });
+  // console.log(pagesAr);
 
-    const renderPages = pagesAr && pagesAr.map((item: any, idx: number) => {
-       
-        let imgSrc = getImage(item.image);
-        let imgTitle = item.title;
-        return (
-            <SwiperSlide className="swiperCardPage" key={`page${idx+1}`}> 
-             
-                <GatsbyImage
-                    image={imgSrc}
-                    alt={imgTitle}
-                />
-            </SwiperSlide>
-        );
+  const renderPages =
+    pagesAr &&
+    pagesAr.map((item: any, idx: number) => {
+      let imgSrc = getImage(item.image);
+      let imgTitle = item.title;
+      return (
+        <SwiperSlide className="swiperCardPage" key={`page${idx + 1}`}>
+          <GatsbyImage image={imgSrc} alt={imgTitle} />
+        </SwiperSlide>
+      );
     });
 
+  // const renderTechnology = technology && technology.map((item, i) => {
+  //     return (<span key={`technology${i}`} >{item.name}</span>)
+  // });
 
-    // const renderTechnology = technology && technology.map((item, i) => {
-    //     return (<span key={`technology${i}`} >{item.name}</span>)
-    // });
+  return (
+    <article className="card">
+      <Link className="card-link" to={projectLink}>
+        <div className="card-header">
+          <h2>{title}</h2>
+        </div>
 
-    return (
-        <article className="card">
-            <Link className="card-link" to={projectLink}>
-                <div className="card-header">
-                    <p>{publicData}</p>
-                    <h2>
-                        {title}
-                    </h2>
-                </div>
+        <div className="card-days">
+          <div className="card-days__count">
+            <span className="card-days__count-number"> {pagesCount} </span>
+            <span className="card-days__count-text">Страниц разработано</span>
+          </div>
+          <div className="card-days__svg">
+            <svg className="half-circle" viewBox="0 0 106 57">
+              <path d="M102 4c0 27.1-21.9 49-49 49S4 31.1 4 4"></path>
+            </svg>
+          </div>
+        </div>
+        <div className="card-pages">
+          <Swiper
+            effect={"cards"}
+            grabCursor={true}
+            modules={[EffectCards]}
+            className="swiperCardPages"
+          >
+            {renderPages}
+          </Swiper>
+        </div>
 
-                <div className="card-days">
-                    <div className="card-days__count">
-                      <span className="card-days__count-number"> {pagesCount} </span> 
-                        <span className="card-days__count-text">
-                        Страниц разработано
-                        </span>
-                        
-                    </div>
-                    <div className="card-days__svg">
-                        <svg className="half-circle" viewBox="0 0 106 57">
-                            <path d="M102 4c0 27.1-21.9 49-49 49S4 31.1 4 4"></path>
-                        </svg>
-                    </div>
-                </div>
-                <div className="card-pages">
-                    <Swiper
-                        effect={"cards"}
-                        grabCursor={true}
-                        modules={[EffectCards]}
-                        className="swiperCardPages"
-                    >
-                        {renderPages}
-                    </Swiper>
-                </div>
-
-                <div className="tags">
-                    {/* {renderTechnology} */}
-                </div>
-            </Link>
-        </article>
-    );
-}
-
+        <div className="tags">{/* {renderTechnology} */}</div>
+      </Link>
+    </article>
+  );
+};
 
 export default Card;
-
-
